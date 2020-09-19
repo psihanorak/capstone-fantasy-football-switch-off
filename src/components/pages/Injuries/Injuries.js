b7df9d149aa1fc1506bdeb97f574fe1506e4e420
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-09-19 15:54:07 -0500

    MyPlayers Can Now Be Displayed

--- src/components/pages/Injuries/Injuries.js
+++ src/components/pages/Injuries/Injuries.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import Injury from '../../../images/injuries.png';
 import './Injuries.scss';
 
 class Injuries extends React.Component {
   render() {
     return (
       <div className="injuries-page">
-        <img src={Injury} alt="" />
       </div>
     );
   }
--- src/components/pages/nflPlayers/nflPlayers.js
+++ src/components/pages/nflPlayers/nflPlayers.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import Players from '../../../images/nflPlayers.png';
 import './nflPlayers.scss';
 
 class nflPlayers extends React.Component {
   render() {
     return (
       <div className="players-page">
-        <img src={Players} alt="" />
       </div>
     );
   }
