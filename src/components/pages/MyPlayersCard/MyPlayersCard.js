776c2726430ad98a4d6e42718b1f0b7219007475
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-09-22 22:01:06 -0500

    Players Can Now Be Updated

--- src/components/pages/MyPlayersCard/MyPlayersCard.js
+++ src/components/pages/MyPlayersCard/MyPlayersCard.js
@@ -9,25 +9,25 @@ import './MyPlayersCard.scss';
 const MyPlayersCard = (props) => {
   const { myPlayer, deleteMyPlayer } = props;
 
-  const locker = `/locker/${myPlayer.id}`;
+  const editLink = `/edit/${myPlayer.id}`;
 
   return (
     <div className="MyPlayersCard" id={myPlayer.id}>
       <div className="card" style={{ width: '20rem' }}>
         <img src={myPlayer.imageUrl} alt="" />
-      <div className="card-body">
-        <h2 className="card-title">{ myPlayer.displayName }</h2>
-        <p className="card-text">Jersey: #{ myPlayer.jersey }</p>
-        <p className="card-text">Team: { myPlayer.team }</p>
-        <p className="card-text">Position: { myPlayer.position }</p>
-        <div className="Start-Remove-btn">
-          <Link to={locker}>
-          <button type="button" className="btn btn-success">Change</button>
-          </Link>
-          <button type="button" className="btn btn-danger" onClick={() => { deleteMyPlayer(myPlayer.id); }}>Remove</button>
+        <div className="card-body">
+          <h2 className="card-title">{ myPlayer.displayName }</h2>
+          <p className="card-text">Jersey: #{ myPlayer.jersey }</p>
+          <p className="card-text">Team: { myPlayer.team }</p>
+          <p className="card-text">Position: { myPlayer.position }</p>
+          <div className="Start-Remove-btn">
+            <Link to={editLink}>
+            <button type="button" className="btn btn-success">Change</button>
+            </Link>
+            <button type="button" className="btn btn-danger" onClick={() => { deleteMyPlayer(myPlayer.id); }}>Remove</button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
